43b52ae99eb6d57fa0e64444486a972166a38892
Addy Osmani <addyosmani@gmail.com>
2014-12-30 22:09:25 +0000

    Add JSDoc comments

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
 var cheerio = require('cheerio');
 
+/**
+ * Export color string (e.g hex color) from the "content" 
+ * portion of the theme-color meta-tag
+ * @param  {String} str HTML String
+ * @return {String}     theme-color string if one is present
+ */
 module.exports = function (str) {
   // Tyical input: <meta name="theme-color" content="#3372DF">
   var $          = cheerio.load(str);
