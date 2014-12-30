1db8415d76e9c5dad3389061348cd9bba17d6ffc
Addy Osmani <addyosmani@gmail.com>
2014-12-30 02:20:54 +0000

    Drop unnecessary dep

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 var cheerio = require('cheerio');
-var trim    = require('trim');
 
 module.exports = function (str) {
   // Tyical input: <meta name="theme-color" content="#3372DF">
   var $          = cheerio.load(str);
   var metatag    = $('meta[name=theme-color]');
   var content    = metatag.attr("content") || '';
-  return trim(content);
+  return content.trim();
 };
