e91cdb54aa0c57d5afd0968b80402daedd639b02
Arthur Verschaeve <contact@arthurverschaeve.be>
2014-12-30 15:54:08 +0100

    Tests: use 2 spaces indentation
    To avoid inconsistencies in the future, an `.editorconfig` file is
    added.

--- test/test.js
+++ test/test.js
@@ -4,31 +4,31 @@ var assert = require('assert');
 var metaThemeColor = require('../');
 
 describe('meta-theme-color node module', function () {
-	it('should return a theme-color extracted from a meta tag', function (done) {
-		assert.equal(metaThemeColor('<meta name="theme-color" content="#3372DF">'), '#3372DF');
-		assert.equal(metaThemeColor('<meta name="theme-color" content="#000">'), '#000');
-		assert.equal(metaThemeColor('<meta name=\'theme-color\' content="#3372DF">'), '#3372DF');
-		assert.equal(metaThemeColor('<meta   name="theme-color" content="#3372DF" >'), '#3372DF');
-		assert.equal(metaThemeColor('<meta   name="theme-color" content=\'#3372DF\' >'), '#3372DF');
-		assert.equal(metaThemeColor('<meta name=\'theme-color\' content=\'#3372DF\'>'), '#3372DF');
-		assert.equal(metaThemeColor('<meta name=\'theme-color\' content=\'red\'>'), 'red');
-		assert.equal(metaThemeColor('<meta content=\'red\' name=\'theme-color\'>'), 'red');
-		assert.equal(metaThemeColor('<meta name="theme-color" content=" #3372DF">'), '#3372DF');
-		assert.equal(metaThemeColor('<meta   name="theme-color" content=" red   ">'), 'red');
-		assert.equal(metaThemeColor('<meta   name="theme-color" content=" rgb(255,0,0)   ">'), 'rgb(255,0,0)');
-		assert.equal(metaThemeColor('<meta name="theme-color" content="rgb(255,0,0)">'), 'rgb(255,0,0)');
-		done();
-	});
+  it('should return a theme-color extracted from a meta tag', function (done) {
+    assert.equal(metaThemeColor('<meta name="theme-color" content="#3372DF">'), '#3372DF');
+    assert.equal(metaThemeColor('<meta name="theme-color" content="#000">'), '#000');
+    assert.equal(metaThemeColor('<meta name=\'theme-color\' content="#3372DF">'), '#3372DF');
+    assert.equal(metaThemeColor('<meta   name="theme-color" content="#3372DF" >'), '#3372DF');
+    assert.equal(metaThemeColor('<meta   name="theme-color" content=\'#3372DF\' >'), '#3372DF');
+    assert.equal(metaThemeColor('<meta name=\'theme-color\' content=\'#3372DF\'>'), '#3372DF');
+    assert.equal(metaThemeColor('<meta name=\'theme-color\' content=\'red\'>'), 'red');
+    assert.equal(metaThemeColor('<meta content=\'red\' name=\'theme-color\'>'), 'red');
+    assert.equal(metaThemeColor('<meta name="theme-color" content=" #3372DF">'), '#3372DF');
+    assert.equal(metaThemeColor('<meta   name="theme-color" content=" red   ">'), 'red');
+    assert.equal(metaThemeColor('<meta   name="theme-color" content=" rgb(255,0,0)   ">'), 'rgb(255,0,0)');
+    assert.equal(metaThemeColor('<meta name="theme-color" content="rgb(255,0,0)">'), 'rgb(255,0,0)');
+    done();
+  });
 
-	it('should return a theme-color extracted from a HTML string', function (done) {
-		assert.equal(metaThemeColor('<html><head><meta name="theme-color" content="#3372DF"></head><body></body></html>'), '#3372DF');
-		done();
-	});
+  it('should return a theme-color extracted from a HTML string', function (done) {
+    assert.equal(metaThemeColor('<html><head><meta name="theme-color" content="#3372DF"></head><body></body></html>'), '#3372DF');
+    done();
+  });
 
-	it('should return an empty color string for non-valid input', function (done) {
-		assert.equal(metaThemeColor('<meta name="theme-color">'), '');
-		assert.equal(metaThemeColor('<meta name>'), '');
-		assert.equal(metaThemeColor('<html></html>'), '');
-		done();
-	});
+  it('should return an empty color string for non-valid input', function (done) {
+    assert.equal(metaThemeColor('<meta name="theme-color">'), '');
+    assert.equal(metaThemeColor('<meta name>'), '');
+    assert.equal(metaThemeColor('<html></html>'), '');
+    done();
+  });
 });
